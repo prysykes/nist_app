90f9719d77b5f292970e08339fbe1f1138b593dd
prysykes <emisykes2000@yahoo.com>
2024-02-05 03:39:12 +0100

    updating database schema

--- pages/views.py
+++ pages/views.py
@@ -3,14 +3,28 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from .upload_form import VideoUploadForm
+from .models import Videos
+from .utils import handle_upload_videos
+from .utils import display_videos
 
-# Create your views here.
+display_videos()
+
+# checked_by = models.CharField(max_length=30, null=True, blank=True)
+#     video = models.FileField(upload_to='videos', verbose_name='Trec Videos')
+#     file_name = models.CharField(max_length=30, null=True, blank=True)
+#     cluster_id = models.CharField(max_length=20)
+#     status = models.BooleanField(default=False)
+#     date_uploaded = models.DateField(auto_now_add=True)
 
 # @login_required(login_url='user_login')
+
+
+
 def index(request):
-    
+    video_upload_form = VideoUploadForm()
     if request.method == 'POST':
-        if request.POST.get('username') != None: #check if the loging form was submitted
+        if request.POST.get('username') != None: #check if the login form was submitted
             print('user logn hit', request.POST)
             username = request.POST.get('username')
             password = request.POST.get('password')
@@ -22,10 +36,15 @@ def index(request):
                 messages.info(request, "Account inactive, login to your email to activate your account")
             return redirect('/')
         else:
-            #handle uploaded images
-            pass
+            video_upload_form = VideoUploadForm(request.POST, request.FILES)
+            uploaded_videos = request.FILES.getlist('video')
+            handle_upload_videos(request, uploaded_videos, video_upload_form)
+            return redirect('/')
+
     else:
-        context = {}
+        context = {
+            'video_upload': video_upload_form
+        }
         return render(request, 'index.html', context)
 
 def sign_up(request):
